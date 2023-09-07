64df70bf04f9c47e7314104400c26890e5cb6efe
Tanmay Siwach <siwachtanmayus@gmail.com>
2023-09-06 21:45:55 -0700

    project connections

--- src/app/projectsData.js
+++ src/app/projectsData.js
@@ -3,5 +3,35 @@ export const projects={
         'name':'RealTime Card Game',
         'description':'Built on TypeScript with React, Node, Apollo GraphQL and Websockets.',
         'url':'/card.png'
+    },
+    102:{
+        'name':'RealTime Card Game',
+        'description':'Built on TypeScript with React, Node, Apollo GraphQL and Websockets.',
+        'url':'/card.png'
+    },
+    103:{
+        'name':'Anti Spotify',
+        'description':'Built on Java Sprinboot, React, Rest, SpotifyAPI and ChatGPT 3.5 Turbo.',
+        'url':'/spotify.png'
+    },
+    104:{
+        'name':'RealTime Card Game',
+        'description':'Built on TypeScript with React, Node, Apollo GraphQL and Websockets.',
+        'url':'/card.png'
+    },
+    105:{
+        'name':'Theta Chi',
+        'description':'Built on NextJS.',
+        'url':'/theta.png'
+    },
+    106:{
+        'name':'RealTime Card Game',
+        'description':'Built on TypeScript with React, Node, Apollo GraphQL and Websockets.',
+        'url':'/card.png'
+    },
+    107:{
+        'name':'The Garage',
+        'description':'Built on NextJS.',
+        'url':'/garage.png'
     }
 }
